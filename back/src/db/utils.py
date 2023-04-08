ca370cfca20647e40e7ff4908a55a056ab24e750
Sergey Natalenko <sergey.natalenko@mail.ru>
2023-04-08 22:57:33 +0300

    Fix models and add relationships

--- back/src/db/utils.py
+++ back/src/db/utils.py
@@ -1,7 +1,4 @@
-from db.base import Base
-from db.engine import async_engine
-
-
 async def create_db_and_tables():
-    async with async_engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with async_engine.begin() as conn:
+    #     await conn.run_sync(Base.metadata.create_all)
+    pass
