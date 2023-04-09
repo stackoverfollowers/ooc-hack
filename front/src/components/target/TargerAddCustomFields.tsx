d1309ecede69cdcdc8bf0c9c019c0c12e589b1e4
forlorn <clickshaq11@gmail.com>
2023-04-09 18:28:37 +0300

    Fix custom fields add

--- front/src/components/target/TargerAddCustomFields.tsx
+++ front/src/components/target/TargerAddCustomFields.tsx
@@ -1,21 +1,22 @@
 import {
+    ActionIcon,
     Button,
     Center,
     Checkbox,
     CloseButton,
     Divider,
+    Grid,
     NumberInput,
     Popover,
     Select,
     Stack,
     Text,
-    TextInput,
-    UnstyledButton
+    TextInput
 } from "@mantine/core";
-import {useState} from "react";
-import {BsChevronCompactLeft} from "react-icons/bs";
+import {Fragment, useState} from "react";
+import {BsChevronLeft} from "react-icons/bs";
 import {FiLayers} from "react-icons/fi";
-import {BiPlus} from "react-icons/bi";
+import {BiPlus, BiTrash} from "react-icons/bi";
 import {IoAlbumsOutline} from "react-icons/io5";
 import {createStyles} from "@mantine/styles";
 
@@ -43,6 +44,10 @@ const useStyles = createStyles(theme => ({
         },
     },
 
+    deleteButton: {
+      backgroundColor: "red"
+    },
+
     close: {
         position: 'absolute',
         right: 14,
@@ -57,7 +62,6 @@ const FieldButton = ({field, onClick}: { field: Field }) => {
             variant="light"
             color="gray"
             leftIcon={<IoAlbumsOutline/>}
-            rightIcon={<IoAlbumsOutline/>}
             styles={{inner: {justifyContent: 'flex-start'}}}
         >
             {field.name}
@@ -75,12 +79,7 @@ type Field = {
 }
 
 const initialFields = [
-    {
-        id: 1,
-        name: 'Поле1',
-        type: "text",
-        value: 'Значение поля1'
-    }
+
 ] satisfies Field[]
 
 type State = 'none' | 'editing' | 'creating'
@@ -121,13 +120,67 @@ const values = {
         label='Выберите значение'
         checked={value}
         onChange={(event) => setValue(event.currentTarget.checked)}
-    />
+    />,
+    'select': (value, setValue) => <SelectComponent value={value} setValue={setValue}/>
+}
+
+
+const SelectComponent = ({value: values, setValue}) => {
+    const {classes} = useStyles()
+
+    return <>
+        <Grid>
+            {values?.map?.((value, index) => {
+                return <Fragment key={value.label + value.key + index}>
+                    <Grid.Col span={5}>
+                        <TextInput
+                            onChange={e => setValue(values.map(inner => {
+                                if (inner.id === value.id) return {...inner, label: e.currentTarget.value}
+                                return inner
+                            }))}
+                            value={value.label}
+                            label='Поле'
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={5}>
+                        <TextInput
+                            onChange={e => setValue(values.map(inner => {
+                                if (inner.id === value.id) return {...inner, value: e.currentTarget.value}
+                                return inner
+                            }))}
+                            value={value.value}
+                            label='Значение'
+                        />
+                    </Grid.Col>
+                    <Grid.Col span={1}>
+                        <ActionIcon
+                            className={classes.deleteButton}
+                            variant='subtle'
+                            onClick={( ) => setValue(values.filter(inner => inner.id !== value.id))}
+                        >
+                            <BiTrash />
+                        </ActionIcon>
+                    </Grid.Col>
+                </Fragment>
+            })}
+        </Grid>
+        <Button
+            onClick={() => setValue([...values, {
+                id: values?.length + 1 || 0,
+                label: '',
+                value: ''
+            }])}
+        >
+            Добавить поля в список
+        </Button>
+    </>
 }
 
 const defaultValues = {
     'text': '',
     'number': '',
-    'checkbox': false
+    'checkbox': false,
+    'select': []
 }
 
 export default function TargetAddCustomFields() {
@@ -148,6 +201,11 @@ export default function TargetAddCustomFields() {
         'none': <>
             <Divider my={8}/>
             <Stack className={classes.members}>
+                {customFields.length === 0 &&
+                    <Text c='dimmed' align='center'>
+                        Здесь пока ничего нет...
+                    </Text>
+                }
                 {customFields.map((field, index) => {
                         return (
                             <FieldButton field={field} key={field.name + index} onClick={
@@ -172,7 +230,7 @@ export default function TargetAddCustomFields() {
             <>
                 <TextInput
                     value={currentEditableField?.name}
-                    onChange={(e) => setCurrentEditableField( {
+                    onChange={(e) => setCurrentEditableField({
                         ...currentEditableField, name: e.currentTarget.value
                     } as Field)}
                     placeholder='Введите название поля'
@@ -180,7 +238,7 @@ export default function TargetAddCustomFields() {
                 />
                 <Select
                     value={currentEditableField?.type || null}
-                    onChange={(e) => setCurrentEditableField( {
+                    onChange={(e) => setCurrentEditableField({
                         ...currentEditableField, type: e, value: defaultValues[e],
                     } as Field)}
                     data={fieldTypes}
@@ -206,7 +264,7 @@ export default function TargetAddCustomFields() {
         'editing': <>
             <TextInput
                 value={currentEditableField?.name}
-                onChange={(e) => setCurrentEditableField( {
+                onChange={(e) => setCurrentEditableField({
                     ...currentEditableField, name: e.currentTarget.value
                 } as Field)}
                 placeholder='Введите название поля'
@@ -214,8 +272,8 @@ export default function TargetAddCustomFields() {
             />
             <Select
                 value={currentEditableField?.type || null}
-                onChange={(e) => setCurrentEditableField( {
-                   ...currentEditableField, type: e, value: defaultValues[e],
+                onChange={(e) => setCurrentEditableField({
+                    ...currentEditableField, type: e, value: defaultValues[e],
                 } as Field)}
                 data={fieldTypes}
                 placeholder='Выберите тип поля'
@@ -223,7 +281,7 @@ export default function TargetAddCustomFields() {
             />
             {
                 values[currentEditableField?.type]?.(currentEditableField?.value, (value) => setCurrentEditableField(
-                    {...currentEditableField, value} as Field
+                    {...currentEditableField, value: value} as Field
                 ))
             }
             <Button
@@ -237,6 +295,16 @@ export default function TargetAddCustomFields() {
                 }}
             >
                 Завершить изменения
+            </Button>
+            <Button
+                leftIcon={<BiTrash/>}
+                onClick={() => {
+                    // проходим по всем кастомным полям и ищем изменяемое
+                    setCustomFields(fields => (fields.filter(field => field.id !== currentEditableField?.id)))
+                    setState('none')
+                }}
+            >
+                Удалить поле
             </Button>
         </>
     }
@@ -267,19 +335,19 @@ export default function TargetAddCustomFields() {
                     theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
             })}
         >
-            <Stack>
-                <UnstyledButton
+                <ActionIcon
                     onClick={() => setState('none')}
                 >
-                    <BsChevronCompactLeft/>
-                </UnstyledButton>
-                <Center>
-                    <Text>Поля пользователя</Text>
-                </Center>
-                <CloseButton
-                    className={classes.close}
-                    onClick={() => setOpened(false)}
-                />
+                    {state !== 'none' && <BsChevronLeft/>}
+                </ActionIcon>
+            <Center>
+                <Text>Поля пользователя</Text>
+            </Center>
+            <CloseButton
+                className={classes.close}
+                onClick={() => setOpened(false)}
+            />
+            <Stack>
                 {states[state]}
             </Stack>
         </Popover.Dropdown>
